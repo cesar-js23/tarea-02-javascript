68fc8a36e6e9a36f3af8146486f798141e4a1dad
Cesar Julian <cejulian23@gmail.com>
2022-08-05 02:39:47 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const lista = [
+    "Cesar",
+    29,
+    true,
+    new Date(1992, 07, 20),
+    {
+      titulo: "El Principito",
+      autoy: " Antoine de Saint-Exupéry",
+      fecha: new Date(1943, 0, 1),
+      url: "https://planetafacil.plenainclusion.org/wp-content/uploads/2019/07/el_principito_lf_1.0.pdf",
+    },
+  ];
